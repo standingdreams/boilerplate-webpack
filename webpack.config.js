4f8d2b9466b7439e0e0e827d5cf138e750a3918b
Douglas Rogers <doug@morrisonagency.com>
2017-12-11 13:52:17 -0500

    added HMR, css and js minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
 /*
-- [ ] Minify JS
-- [ ] Minify SCSS
-- [ ] Hot Module Replacement
+- [x] Minify JS
+- [x] Minify SCSS
+- [ ] Hot Module Replacement with extracted CSS
 - [ ] Tree Shaking
-- [ ] Separate Environments (dev and prod)
-- [ ] Autoprefix CSS
+- [x] Separate Environments (dev and prod)
+- [x] Autoprefix CSS
+- [ ] Sourcemap
  */
 
 const webpack = require('webpack');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssets = require('optimize-css-assets-webpack-plugin');
+
 const extractCSS = new ExtractTextPlugin('/_assets/css/style.css');
 
 let config = {
@@ -30,13 +34,23 @@ let config = {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
           fallback: "style-loader",
-          use: ['css-loader', 'sass-loader']
+          use: [
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'postcss-loader',
+            },
+            {
+              loader: 'sass-loader',
+            }
+          ]
         })
       }
     ],
   },
   plugins: [
-    new ExtractTextPlugin('_assets/css/style.css')
+    new ExtractTextPlugin('_assets/css/styles.css')
   ],
   devServer: {
     contentBase: path.resolve(__dirname, './'),
@@ -45,3 +59,10 @@ let config = {
 };
 
 module.exports = config;
+
+if(process.env.NODE_ENV === 'production') {
+  module.exports.plugins.push(
+    new webpack.optimize.UglifyJsPlugin(),
+    new OptimizeCSSAssets(),
+  )
+}
